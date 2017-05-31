00ca978f7ef1f3e587dad4be4541761d74de0f38
Yuri Astrakhan <yurik@wikimedia.org>
2017-05-31 15:18:14 -0400

    rudimentary local post & demo

--- public/vega_view/vega_view.js
+++ public/vega_view/vega_view.js
@@ -7,7 +7,52 @@ export function createVegaView(el, spec) {
   const getWidth = () => $el.width() - 100;
   const getHeight = () => $el.height() - 100;
 
-  const view = new Vega.View(Vega.parse(spec))
+  const opts = {
+    /**
+     * ... the loader instance to use for data file loading. A
+     * loader object must provide a "load" method for loading files and a
+     * "sanitize" method for checking URL/filename validity. Both methods
+     * should accept a URI and options hash as arguments, and return a Promise
+     * that resolves to the loaded file contents (load) or a hash containing
+     * sanitized URI data with the sanitized url assigned to the "href" property
+     * (sanitize).
+     */
+    loader: {
+      load: (uri, opts) => {
+        return new Promise((accept, reject) => {
+          switch (opts.context) {
+            case 'dataflow':
+              // FIXME!!! - should use standard Kibana requester
+              let header = uri._header;
+              if (header !== undefined) {
+                delete uri._header;
+              } else {
+                header = {index: '*'};
+              }
+              $.ajax({
+                type: 'POST',
+                url: 'https://localhost:5601/tla/elasticsearch/_msearch',
+                dataType: 'json',
+                data: JSON.stringify(header) + '\n' + JSON.stringify(uri) + '\n',
+                headers: {
+                  'content-type': 'application/x-ndjson',
+                  'accept': 'application/json, text/plain, */*'
+                },
+                success: (result) => {
+                  accept(result);
+                }
+              });
+              break;
+          }
+        });
+      },
+      sanitize: (uri, opts) => {
+        return Promise.resolve({});
+      }
+    }
+  };
+
+  const view = new Vega.View(Vega.parse(spec), opts)
     .logLevel(Vega.Warn)
     .renderer('canvas')
     .padding({ left: 0, right: 0, top: 0, bottom: 0 })
@@ -25,16 +70,16 @@ export function createVegaView(el, spec) {
         .run();
     }
 
-    setData(data) {
-      const changeset = Vega.changeset()
-        .remove(view.data('esResp'))
-        .insert(data);
-
-      view
-        .change('esResp', changeset)
-        .run();
-    }
-
+    // setData(data) {
+    //   const changeset = Vega.changeset()
+    //     .remove(view.data('esResp'))
+    //     .insert(data);
+    //
+    //   view
+    //     .change('esResp', changeset)
+    //     .run();
+    // }
+    //
     destroy() {
       view.finalize();
       $el.empty();
