163a4c71b792b0d85d34202a0717e1a689914a4b
Yuri Astrakhan <yurik@wikimedia.org>
2017-06-01 01:19:11 -0400

    migrating to Vega embed

--- public/vega_view/vega_view.js
+++ public/vega_view/vega_view.js
@@ -1,5 +1,7 @@
 import $ from 'jquery';
-import * as Vega from 'vega';
+
+// Strangely, importing from vega-embed directly doesn't compile
+import embed from 'vega-embed/src/embed';
 
 export function createVegaView(el, spec) {
   const $el = $(el);
@@ -8,66 +10,72 @@ export function createVegaView(el, spec) {
   const getHeight = () => $el.height() - 100;
 
   const opts = {
-    /**
-     * ... the loader instance to use for data file loading. A
-     * loader object must provide a "load" method for loading files and a
-     * "sanitize" method for checking URL/filename validity. Both methods
-     * should accept a URI and options hash as arguments, and return a Promise
-     * that resolves to the loaded file contents (load) or a hash containing
-     * sanitized URI data with the sanitized url assigned to the "href" property
-     * (sanitize).
-     */
-    loader: {
-      load: (uri, opts) => {
-        return new Promise((accept, reject) => {
-          switch (opts.context) {
-            case 'dataflow':
-              // FIXME!!! - should use standard Kibana requester
-              let header = uri._header;
-              if (header !== undefined) {
-                delete uri._header;
-              } else {
-                header = {index: '*'};
-              }
-              $.ajax({
-                type: 'POST',
-                url: 'https://localhost:5601/tla/elasticsearch/_msearch',
-                dataType: 'json',
-                data: JSON.stringify(header) + '\n' + JSON.stringify(uri) + '\n',
-                headers: {
-                  'content-type': 'application/x-ndjson',
-                  'accept': 'application/json, text/plain, */*'
-                },
-                success: (result) => {
-                  accept(result);
+    viewConfig: {
+      /**
+       * ... the loader instance to use for data file loading. A
+       * loader object must provide a "load" method for loading files and a
+       * "sanitize" method for checking URL/filename validity. Both methods
+       * should accept a URI and options hash as arguments, and return a Promise
+       * that resolves to the loaded file contents (load) or a hash containing
+       * sanitized URI data with the sanitized url assigned to the "href" property
+       * (sanitize).
+       */
+      loader: {
+        load: (uri, opts) => {
+          return new Promise((accept, reject) => {
+            switch (opts.context) {
+              case 'dataflow':
+                // FIXME!!! - should use standard Kibana requester
+                let body = uri.body;
+                if (body === undefined) {
+                  throw new Error('Missing request body');
                 }
-              });
-              break;
-          }
-        });
-      },
-      sanitize: (uri, opts) => {
-        return Promise.resolve({});
+                delete uri.body;
+                $.ajax({
+                  type: 'POST',
+                  url: 'https://localhost:5601/uvx/elasticsearch/_msearch',
+                  dataType: 'json',
+                  data: JSON.stringify(uri) + '\n' + JSON.stringify(body) + '\n',
+                  headers: {
+                    'kbn-name': 'kibana',
+                    'kbn-version': '6.0.0-alpha2',
+                    'content-type': 'application/x-ndjson',
+                    'accept': 'application/json, text/plain, */*'
+                  },
+                  success: (result) => {
+                    accept(result);
+                  }
+                });
+                break;
+            }
+          });
+        },
+        sanitize: (uri, opts) => {
+          return Promise.resolve({});
+        }
       }
-    }
+    },
+    "width": getWidth(),
+    "height": getHeight(),
+    "padding": {left: 0, right: 0, top: 0, bottom: 0}
   };
 
-  const view = new Vega.View(Vega.parse(spec), opts)
-    .logLevel(Vega.Warn)
-    .renderer('canvas')
-    .padding({ left: 0, right: 0, top: 0, bottom: 0 })
-    .initialize($el.get(0))
-    .width(getWidth())
-    .height(getHeight())
-    .hover()
-    .run();
+  let viewP = new Promise((accept, reject) => {
+    embed($el.get(0), spec, opts, (err, v) => {
+      if (err)
+        reject(err);
+      else
+        accept(v);
+    });
+  });
 
   class VegaView {
     resize() {
-      view
-        .width(getWidth())
-        .height(getHeight())
-        .run();
+      viewP.then(v =>
+        v.view
+          .width(getWidth())
+          .height(getHeight())
+          .run());
     }
 
     // setData(data) {
@@ -81,7 +89,7 @@ export function createVegaView(el, spec) {
     // }
     //
     destroy() {
-      view.finalize();
+      viewP.then(v=> v.view.finalize());
       $el.empty();
     }
   }
