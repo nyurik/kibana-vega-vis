73fdcd117623fd32d7af289bb3796ce4f3c270a6
Yuri Astrakhan <yurik@wikimedia.org>
2017-07-07 18:07:58 -0400

    Revert "start migrating to new visualization plugin for 6.0"
    This reverts commit 61003da91950c796d1ef39dd57c4bafa6be2cdf5.

--- public/vega_vis.vis_type.js
+++ public/vega_vis.vis_type.js
@@ -1,6 +1,7 @@
-import { CATEGORY } from 'ui/vis/vis_category';
-import { VisFactoryProvider } from 'ui/vis/vis_factory';
+import { TemplateVisTypeProvider } from 'ui/template_vis_type/template_vis_type';
+// import { VisSchemasProvider } from 'ui/vis/schemas';
 import { VisTypesRegistryProvider } from 'ui/registry/vis_types';
+import { VisVisTypeProvider } from 'ui/vis/vis_type';
 
 import './vega_vis.directive';
 import './vega_vis_editor.directive';
@@ -8,36 +9,33 @@ import './vega_vis_editor.directive';
 import demoSpec from '!!raw-loader!./examples/demo.spec.json';
 
 // register the provider with the visTypes registry
-VisTypesRegistryProvider.register(VegaVisProvider);
-
-function VegaVisProvider(Private) {
-  const VisFactory = Private(VisFactoryProvider);
+VisTypesRegistryProvider.register(function MetricVisProvider(Private) {
+  const VisType = Private(VisVisTypeProvider);
+  const TemplateVisType = Private(TemplateVisTypeProvider);
+  // const VisSchemas = Private(VisSchemasProvider);
 
   // return the visType object, which kibana will use to display and configure new
   // Vis object of this type.
-  return VisFactory.createAngularVisualization({
+  return new TemplateVisType({
     name: 'vega',
     title: 'Vega Vis',
     description: '',
     icon: 'fa-code',
-    visConfig: {
-      template: `<vega-vis vis="vis" es-response="esResponse"></vega-vis>`,
-    },
-    editorConfig: {
-      optionsTemplate: `<vega-vis-editor
-                          vis="vis"
-                          persist-app-state="state.save(true)"
-                          ui-state="uiState"
-                          ></vega-vis-editor>`
-    },
+    template: `<vega-vis vis="vis" es-response="esResponse"></vega-vis>`,
     params: {
       defaults: {
         spec: '' // FIXME!!!!!!!!!!!!!!!!!!  ---  demoSpec
       },
+      editor: `<vega-vis-editor
+vis="vis"
+persist-app-state="state.save(true)"
+ui-state="uiState"
+></vega-vis-editor>`
     },
-    requestHandler: 'none',
+    // implementsRenderComplete: true,
+    requiresSearch: false,
     requiresTimePicker: true,
     fullEditor: true,
-    category: CATEGORY.OTHER
+    category: VisType.CATEGORY.OTHER
   });
-};
+});
