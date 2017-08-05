48cc863fb195d5e67ee6eb4ccc452231c9161932
Yuri Astrakhan <yurik@wikimedia.org>
2017-08-04 22:57:05 -0400

    fixed dashboard context auto-update

--- public/vega_vis.controller.js
+++ public/vega_vis.controller.js
@@ -1,9 +1,6 @@
 import { ResizeCheckerProvider } from 'ui/resize_checker';
-// import { Notifier } from 'ui/notify';
 import { dashboardContextProvider } from 'plugins/kibana/dashboard/dashboard_context';
-
 import { VegaView } from './vega_view';
-// import moment from 'moment';
 
 import hjson from 'hjson';
 
@@ -32,9 +29,7 @@ export function createVegaVisController(Private, /*$scope,*/ timefilter, es, ser
     link($scope, $el/*, $attr*/) {
       const resizeChecker = new ResizeChecker($el);
 
-      $scope.timefilter = timefilter;
-      $scope.dashboardContext = dashboardContext;
-      $scope.$watchMulti(['=vega.vis.params', '=timefilter', '=dashboardContext', '=vis.params.expression', '=vis.params.interval', '=vega.vis.params.expression', '=vega.vis.params.interval'], async () => {
+      const createGraph = async () => {
         this.messages = [];
 
         // FIXME!!  need to debounce editor changes
@@ -51,7 +46,17 @@ export function createVegaVisController(Private, /*$scope,*/ timefilter, es, ser
         }
 
         resizeChecker.modifySizeWithoutTriggeringResize(() => this.vegaView.resize());
-      });
+      };
+
+      $scope.timefilter = timefilter;
+      $scope.$watchMulti(
+        [
+          '=vega.vis.params',
+          '=timefilter',
+          { get: dashboardContext, deep: true }
+        ],
+        createGraph
+      );
 
       resizeChecker.on('resize', () => {
         resizeChecker.modifySizeWithoutTriggeringResize(() => this.vegaView.resize());
@@ -61,47 +66,6 @@ export function createVegaVisController(Private, /*$scope,*/ timefilter, es, ser
         this.vegaView.destroy().catch(error => this.onError(error));
       });
     }
-    //
-    // onEsResponse(vis, esResponse) {
-    //   if (!this.vegaView) {
-    //     throw new Error('esResponse provided before vegaView was initialized');
-    //   }
-    //
-    //   if (!vis || !esResponse) {
-    //     this.vegaView.setData([]);
-    //     return;
-    //   }
-    //
-    //   const { columns, rows } = tabify(vis, esResponse, {
-    //     canSplit: false,
-    //     partialRows: true,
-    //     minimalColumns: false,
-    //     // metricsForAllBuckets: false,
-    //     asAggConfigResults: false,
-    //   });
-    //
-    //   const aggTypeCounters = {};
-    //   const columnNames = columns.map(col => {
-    //     const typeName = col.aggConfig.type.type;
-    //     const count = (aggTypeCounters[typeName] || 0) + 1;
-    //     aggTypeCounters[typeName] = count;
-    //     return `${typeName}${count}`;
-    //   });
-    //
-    //   const vegaTable = rows.map(row => {
-    //     return columns.reduce((acc, column, i) => {
-    //       let value = row[i];
-    //       const field = column.aggConfig.getField();
-    //       if (field && field.type === 'date') {
-    //         value = moment(value);
-    //       }
-    //       acc[columnNames[i]] = value;
-    //       return acc;
-    //     }, {});
-    //   });
-    //
-    //   this.vegaView.setData(vegaTable);
-    // }
   }
 
   return new VegaVisController();
